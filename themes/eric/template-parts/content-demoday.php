8c20693e9d58bf96210fd261f9440fde383f500b
Manoj Kumar <talk2manoj@gmail.com>
2017-03-09 18:46:13 +0530

    updated iframe width

--- themes/eric/template-parts/content-demoday.php
+++ themes/eric/template-parts/content-demoday.php
@@ -8,21 +8,15 @@
     <h2 class="page-heading"><?php echo get_custom_post_title();?></h2>
     <div class="entry-content">
       <?php the_content();?>
-      <div class="row">
-        <div class="col-lg-10 col-md-12 col-lg-offset-1">
-          <?php
-            $livestream_url = get_field('live_stream_url');
-            if($livestream_url && strlen($livestream_url) > 10) {
-              echo '<div class="video-wrapper livestream-wrapper"><iframe src="'.$livestream_url.'" frameborder="0" allowfullscreen></iframe></div>';
-            } else {
-              echo '<img src="'.THEME_PATH.'/images/video-placeholder-pre-demo-day.png" alt="Video placeholder pre demo day" class="img-fluid" />';
-            }
-          ?>
-        </div>
-      </div>
-      
+      <?php
+        $livestream_url = get_field('live_stream_url');
+        if($livestream_url && strlen($livestream_url) > 10) {
+          echo '<div class="video-wrapper livestream-wrapper"><iframe src="'.$livestream_url.'" frameborder="0" allowfullscreen></iframe></div>';
+        } else {
+          echo '<img src="'.THEME_PATH.'/images/video-placeholder-pre-demo-day.png" alt="Video placeholder pre demo day" class="img-fluid" />';
+        }
+      ?>
       <?php echo eric_demoday_program();?>
-      
     </div>
     
   </article><!-- #post-## -->
